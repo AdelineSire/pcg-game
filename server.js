2dfc6d6b34d39e6f6a6642c639c3ce9412226c85
Adeline <adeline.sire@gmail.com>
2021-12-06 12:55:42 +0100

    Sets up server and installs dependencies

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+// App
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Controllers
+
+// Routes
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
