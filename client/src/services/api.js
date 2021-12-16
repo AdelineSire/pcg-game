862d00938b698c407e84dbd0816aa468c393e625
Adeline <adeline.sire@gmail.com>
2021-12-16 23:19:48 +0100

    Changes api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
-import API_URL from './config';
 
 const resData = (res) => {
 	return res.data;
 };
 
 const getShortClass = (id) => {
-	return axios.get(API_URL + 'short/' + id).then(resData);
+	return axios.get('short/' + id).then(resData);
 };
 const getBaseClass = (id) => {
-	return axios.get(API_URL + 'base/' + id).then(resData);
+	return axios.get('base/' + id).then(resData);
 };
 
 export { getShortClass, getBaseClass };
