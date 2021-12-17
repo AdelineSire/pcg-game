72b2dbde573810201ebc0b065591c0896622b856
Adeline <adeline.sire@gmail.com>
2021-12-17 08:35:52 +0100

    Adds logs to debug

--- client/src/components/core/ItemsList.jsx
+++ client/src/components/core/ItemsList.jsx
@@ -4,6 +4,7 @@ import { StyledList } from './styled';
 import Item from './Item';
 
 const ItemsList = ({ items, handleClick }) => {
+	console.log('items in ItemsList', items);
 	return (
 		<StyledList>
 			{items.map((item) => (
