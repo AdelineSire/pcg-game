c11d9f14ed4d8aa18457f6400386836803ea4952
Adeline <adeline.sire@gmail.com>
2022-02-16 14:30:29 +0100

    Filters account id > 9

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,7 +7,9 @@ const getBaseClass = (req, res) => {
 	const base = require('../data/base.json');
 	const classNum = req.params.id;
 
-	const accounts = base.filter((obj) => String(obj._id).charAt(0) === classNum);
+	const accounts = base
+		.filter((obj) => String(obj._id).charAt(0) === classNum)
+		.filter((obj) => obj._id > 9);
 	const sample = accounts.slice(0, 8);
 	const ids = randomize(addPropertiesToItems(getListByKey(sample, '_id')));
 	const names = randomize(addPropertiesToItems(getListByKey(sample, 'name')));
